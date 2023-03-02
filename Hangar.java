60c91a61194baed355f2132bc8ff2603ce81c64c
himedsoso <himed.abidar@gmail.com>
2023-03-02 14:09:29 +0100

    first commit

---
+++ Hangar.java
@@ -0,0 +1,12 @@
+public class Hangar {
+    public static void main(String[] args) {
+    
+        Car car1 = new Car (" Megane");
+		Boat boat1 = new Boat ("Titanic");
+
+		
+		System.out.println(car1.doStuff());
+		System.out.println(boat1.doStuff());
+    }
+
+}
---
+++ Vehicle.java
@@ -0,0 +1,30 @@
+public abstract class Vehicle {
+
+     private String brand;  
+     private int kilometers;
+
+
+public Vehicle(String brand) {
+    this.brand = brand;
+    this.kilometers = 0;
+
+}     
+
+public String getBrand() {
+    return this.brand;
+}
+
+public void setBrand(String brand) {
+    this.brand = brand;
+}
+
+public int getKilometers() {
+    return this.kilometers;
+}
+
+public void setKilometer(int kilometers) {
+    this.kilometers = kilometers;
+}
+
+public abstract String doStuff();
+}
